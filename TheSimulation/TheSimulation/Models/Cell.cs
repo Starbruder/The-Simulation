7f2f66a41d478043619e32439a559c6bbcee4c28
STARBRUDER\Nik <starbruder.mc@gmail.com>
2025-12-15 22:22:06 +0100

    Overritten GetHashcode Method () for Cell-Struct & performance

--- TheSimulation/TheSimulation/Models/Cell.cs
+++ TheSimulation/TheSimulation/Models/Cell.cs
@@ -1,3 +1,6 @@
 ﻿namespace TheSimulation.Models;
 
-public readonly record struct Cell(int X, int Y);
+public readonly record struct Cell(int X, int Y)
+{
+	public override int GetHashCode() => HashCode.Combine(X, Y);
+}
