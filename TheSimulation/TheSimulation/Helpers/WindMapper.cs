d5b8246a58fc3ebec29c731c7bf7d02ff350e48b
dhi@gecko.de <dhi@gecko.de>
2025-12-18 08:56:30 +0100

    Adds wind direction conversion from angle for future wind-features (UI)

--- TheSimulation/TheSimulation/Helpers/WindMapper.cs
+++ TheSimulation/TheSimulation/Helpers/WindMapper.cs
@@ -13,4 +13,22 @@ public static class WindMapper
             WindDirection.West => new(-1, 0),
             _ => new(0, 0)
         };
+
+    public static WindDirection ToWindDirection(this float angle)
+    {
+        angle = NormalizeWindAngleToMax360Degrees(angle);
+        var snapped = SnapToNearest45Degrees(angle);
+        return (WindDirection)snapped;
+    }
+
+    private static int SnapToNearest45Degrees(float angle)
+    {
+        return (int)(MathF.Round(angle / 45f) * 45f) % 360;
+    }
+
+    private static float NormalizeWindAngleToMax360Degrees(float angle)
+    {
+        angle = (angle % 360 + 360) % 360;
+        return angle;
+    }
 }
