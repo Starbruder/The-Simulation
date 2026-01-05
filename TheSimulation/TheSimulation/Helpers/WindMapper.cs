b6919cf8617ab8bb50d39066646a06bcd1c6e8f2
STARBRUDER\Nik <starbruder.mc@gmail.com>
2026-01-05 14:10:45 +0100

    Fixing winddirections to let the wind go FROM the dir thats choosen, instead of TO the dir thats choosen

--- TheSimulation/TheSimulation/Helpers/WindMapper.cs
+++ TheSimulation/TheSimulation/Helpers/WindMapper.cs
@@ -7,10 +7,10 @@ public static class WindMapper
     public static Vector GetWindVector(WindDirection direction) =>
         direction switch
         {
-            WindDirection.North => new(0, -1),
-            WindDirection.South => new(0, 1),
-            WindDirection.East => new(1, 0),
-            WindDirection.West => new(-1, 0),
+            WindDirection.North => new(0, 1),
+            WindDirection.South => new(0, -1),
+            WindDirection.East => new(-1, 0),
+            WindDirection.West => new(1, 0),
             _ => new(0, 0)
         };
 
