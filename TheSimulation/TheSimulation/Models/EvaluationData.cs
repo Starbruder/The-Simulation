f4f3cf4d78dd625311358863909f980e4ef6873a
STARBRUDER\Nik <starbruder.mc@gmail.com>
2026-01-06 21:41:31 +0100

    Refactored EvaluetionData as record-class

--- TheSimulation/TheSimulation/Models/EvaluationData.cs
+++ TheSimulation/TheSimulation/Models/EvaluationData.cs
@@ -1,15 +1,20 @@
 ﻿namespace TheSimulation;
 
-public sealed class EvaluationData
+public sealed record EvaluationData
+(
+    uint TotalGrownTrees,
+    uint TotalBurnedTrees,
+    int ActiveTrees,
+    int MaxTreesPossible,
+    TimeSpan Runtime,
+    List<(TimeSpan Time, uint Grown, uint Burned)> History
+)
 {
-    public uint TotalGrownTrees { get; init; }
-    public uint TotalBurnedTrees { get; init; }
-    public int ActiveTrees { get; init; }
-    public int MaxTreesPossible { get; init; }
-    public TimeSpan Runtime { get; init; }
+    public double BurnedPercentage => MaxTreesPossible > 0
+        ? (double)TotalBurnedTrees / MaxTreesPossible * 100
+        : 0;
 
-    public List<(TimeSpan Time, uint Grown, uint Burned)> History { get; init; } = [];
-
-    public double BurnedPercentage => MaxTreesPossible > 0 ? (double)TotalBurnedTrees / MaxTreesPossible * 100 : 0;
-    public double TreeDensityPercentage => MaxTreesPossible > 0 ? (double)ActiveTrees / MaxTreesPossible * 100 : 0;
+    public double TreeDensityPercentage => MaxTreesPossible > 0
+        ? (double)ActiveTrees / MaxTreesPossible * 100
+        : 0;
 }
