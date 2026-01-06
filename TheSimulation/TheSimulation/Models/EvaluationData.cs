ea521a6870a27ff3ad0c69a0032ea64829a2a9fb
STARBRUDER\Nik <starbruder.mc@gmail.com>
2026-01-06 21:25:59 +0100

    Added DataModel for EvalualtionData or „Auswertung“

---
+++ TheSimulation/TheSimulation/Models/EvaluationData.cs
@@ -0,0 +1,15 @@
+﻿namespace TheSimulation;
+
+public sealed class EvaluationData
+{
+    public uint TotalGrownTrees { get; init; }
+    public uint TotalBurnedTrees { get; init; }
+    public int ActiveTrees { get; init; }
+    public int MaxTreesPossible { get; init; }
+    public TimeSpan Runtime { get; init; }
+
+    public List<(TimeSpan Time, uint Grown, uint Burned)> History { get; init; } = [];
+
+    public double BurnedPercentage => MaxTreesPossible > 0 ? (double)TotalBurnedTrees / MaxTreesPossible * 100 : 0;
+    public double TreeDensityPercentage => MaxTreesPossible > 0 ? (double)ActiveTrees / MaxTreesPossible * 100 : 0;
+}
