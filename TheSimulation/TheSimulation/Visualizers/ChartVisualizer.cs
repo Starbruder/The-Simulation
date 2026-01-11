122aa716fbd29214eb09c493e60fd6d49030a156
STARBRUDER\Nik <starbruder.mc@gmail.com>
2026-01-11 14:16:27 +0100

    Chart-Refactorings: Adding ChartVisualizer.cs and moved logic there from EvaluationWindow

---
+++ TheSimulation/TheSimulation/Visualizers/ChartVisualizer.cs
@@ -0,0 +1,83 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Legends;
+using OxyPlot.Series;
+
+namespace TheSimulation;
+
+public static class ChartVisualizer
+{
+    public static PlotModel CreateLineChart(string title, string xAxisTitle, string yAxisTitle)
+    {
+        var model = new PlotModel
+        {
+            Title = title
+        };
+
+        model.Legends.Add(new Legend
+        {
+            IsLegendVisible = true,
+            LegendPlacement = LegendPlacement.Inside,
+            LegendPosition = LegendPosition.TopLeft
+        });
+
+        model.Axes.Add(new LinearAxis
+        {
+            Position = AxisPosition.Bottom,
+            Title = xAxisTitle
+        });
+
+        model.Axes.Add(new LinearAxis
+        {
+            Position = AxisPosition.Left,
+            Title = yAxisTitle
+        });
+
+        return model;
+    }
+
+    public static LineSeries CreateLineSeries(string title, OxyColor color, LineStyle lineStyle = LineStyle.Solid, double thickness = 1)
+    {
+        return new LineSeries
+        {
+            Title = title,
+            Color = color,
+            LineStyle = lineStyle,
+            StrokeThickness = thickness
+        };
+    }
+
+    /// <summary>
+    /// Erstellt eine horizontale Linie für den Durchschnitt einer Serie.
+    /// </summary>
+    /// <param name="history">Liste der History-Einträge.</param>
+    /// <param name="title">Titel der Linie.</param>
+    /// <param name="color">Farbe der Linie.</param>
+    /// <returns>LineSeries für die Durchschnittslinie.</returns>
+    public static LineSeries CreateAverageLine
+        (List<(TimeSpan Time, uint Grown, uint Burned)> history, string title, OxyColor color)
+    {
+        var sum = 0f;
+        foreach (var h in history)
+        {
+            sum += (int)h.Grown - (int)h.Burned;
+        }
+        var average = sum / history.Count;
+
+        var line = new LineSeries
+        {
+            Title = title,
+            Color = color,
+            StrokeThickness = 2,
+            LineStyle = LineStyle.Dash
+        };
+
+        var startTime = history[0].Time.TotalSeconds;
+        var endTime = history[^1].Time.TotalSeconds;
+
+        line.Points.Add(new(startTime, average));
+        line.Points.Add(new(endTime, average));
+
+        return line;
+    }
+}
--- TheSimulation/TheSimulation/Windows/EvaluationWindow.xaml.cs
+++ TheSimulation/TheSimulation/Windows/EvaluationWindow.xaml.cs
@@ -1,7 +1,4 @@
 ﻿using OxyPlot;
-using OxyPlot.Axes;
-using OxyPlot.Legends;
-using OxyPlot.Series;
 using System.Windows;
 
 namespace TheSimulation;
@@ -30,48 +27,18 @@ public sealed partial class EvaluationWindow : Window
 
     private void DrawSimulationChart()
     {
-        var model = new PlotModel
-        {
-            Title = "Grown / Burned Trees"
-        };
+        var model = ChartVisualizer.CreateLineChart("Grown / Burned Trees", "Time (s)", "Trees");
 
-        model.Legends.Add(new Legend
-        {
-            IsLegendVisible = true,
-            LegendPlacement = LegendPlacement.Inside,
-            LegendPosition = LegendPosition.TopLeft,
-        });
+        var grown =
+            ChartVisualizer.CreateLineSeries("Total Grown Trees", OxyColors.Green);
+        var burned =
+            ChartVisualizer.CreateLineSeries("Total Burned Trees", OxyColors.Red);
 
-
-        model.Axes.Add(new LinearAxis
+        foreach (var historySnapshot in data.History)
         {
-            Position = AxisPosition.Bottom,
-            Title = "Time (s)"
-        });
-
-        model.Axes.Add(new LinearAxis
-        {
-            Position = AxisPosition.Left,
-            Title = "Trees"
-        });
-
-        var grown = new LineSeries
-        {
-            Title = "Total Grown Trees",
-            Color = OxyColors.Green
-        };
-
-        var burned = new LineSeries
-        {
-            Title = "Total Burned Trees",
-            Color = OxyColors.Red
-        };
-
-        foreach (var history in data.History)
-        {
-            var t = history.Time.TotalSeconds;
-            grown.Points.Add(new(t, history.Grown));
-            burned.Points.Add(new(t, history.Burned));
+            var t = historySnapshot.Time.TotalSeconds;
+            grown.Points.Add(new(t, historySnapshot.Grown));
+            burned.Points.Add(new(t, historySnapshot.Burned));
         }
 
         model.Series.Add(grown);
@@ -82,88 +49,25 @@ public sealed partial class EvaluationWindow : Window
 
     private void DrawActiveTreesChart()
     {
-        var model = new PlotModel
-        {
-            Title = "Active Trees"
-        };
+        var model =
+            ChartVisualizer.CreateLineChart("Active Trees", "Time (s)", "Trees");
 
-        model.Legends.Add(new Legend
-        {
-            IsLegendVisible = true,
-            LegendPlacement = LegendPlacement.Inside,
-            LegendPosition = LegendPosition.TopLeft
-        });
+        var active =
+            ChartVisualizer.CreateLineSeries("Active Trees", OxyColors.DarkGreen);
 
-        model.Axes.Add(new LinearAxis
+        foreach (var h in data.History)
         {
-            Position = AxisPosition.Bottom,
-            Title = "Time (s)"
-        });
-
-        model.Axes.Add(new LinearAxis
-        {
-            Position = AxisPosition.Left,
-            Title = "Trees",
-            //Maximum = data.MaxTreesPossible // direkt vorskaliert
-        });
-
-        var active = new LineSeries
-        {
-            Title = "Active Trees",
-            Color = OxyColors.DarkGreen
-        };
-
-        var sumActive = 0f;
-
-        foreach (var history in data.History)
-        {
-            var activeTrees = (int)history.Grown - (int)history.Burned;
-            sumActive += activeTrees;
-            active.Points.Add(
-                new(history.Time.TotalSeconds, activeTrees)
-            );
+            var activeCount = (int)h.Grown - (int)h.Burned;
+            active.Points.Add(new(h.Time.TotalSeconds, activeCount));
         }
 
         model.Series.Add(active);
 
         model.Series.Add(
-            CreateAverageLine(data.History, "Average Active Trees", OxyColors.Orange)
+            ChartVisualizer.CreateAverageLine(
+                data.History, "Average Active Trees", OxyColors.Orange)
         );
 
         ActiveTreesPlot.Model = model;
-    }
-
-    /// <summary>
-    /// Erstellt eine horizontale Linie für den Durchschnitt einer Serie.
-    /// </summary>
-    /// <param name="history">Liste der History-Einträge.</param>
-    /// <param name="title">Titel der Linie.</param>
-    /// <param name="color">Farbe der Linie.</param>
-    /// <returns>LineSeries für die Durchschnittslinie.</returns>
-    private static LineSeries CreateAverageLine
-        (List<(TimeSpan Time, uint Grown, uint Burned)> history, string title, OxyColor color)
-    {
-        var sum = 0f;
-        foreach (var h in history)
-        {
-            sum += (int)h.Grown - (int)h.Burned;
-        }
-        var average = sum / history.Count;
-
-        var line = new LineSeries
-        {
-            Title = title,
-            Color = color,
-            StrokeThickness = 2,
-            LineStyle = LineStyle.Dash
-        };
-
-        var startTime = history[0].Time.TotalSeconds;
-        var endTime = history[^1].Time.TotalSeconds;
-
-        line.Points.Add(new(startTime, average));
-        line.Points.Add(new(endTime, average));
-
-        return line;
     }
 }
