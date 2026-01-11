0fb670d09a76625b7dedc31330caee40c9c01bcf
STARBRUDER\Nik <starbruder.mc@gmail.com>
2026-01-11 14:21:29 +0100

    Adding (more) documentation to / for ChartVisulalizer.cs

--- TheSimulation/TheSimulation/Visualizers/ChartVisualizer.cs
+++ TheSimulation/TheSimulation/Visualizers/ChartVisualizer.cs
@@ -5,9 +5,20 @@ using OxyPlot.Series;
 
 namespace TheSimulation;
 
+/// <summary>
+/// Statische Hilfsklasse zum Erstellen und Visualisieren von OxyPlot-Diagrammen.
+/// Kapselt die Erstellung von PlotModels, LineSeries und Durchschnittslinien.
+/// </summary>
 public static class ChartVisualizer
 {
-    public static PlotModel CreateLineChart(string title, string xAxisTitle, string yAxisTitle)
+	/// <summary>
+	/// Erstellt ein neues Liniendiagramm mit Achsen und einer Legende.
+	/// </summary>
+	/// <param name="title">Titel des Diagramms.</param>
+	/// <param name="xAxisTitle">Titel der X-Achse.</param>
+	/// <param name="yAxisTitle">Titel der Y-Achse.</param>
+	/// <returns>Ein neues <see cref="PlotModel"/> mit konfigurierten Achsen und Legende.</returns>
+	public static PlotModel CreateLineChart(string title, string xAxisTitle, string yAxisTitle)
     {
         var model = new PlotModel
         {
@@ -36,7 +47,15 @@ public static class ChartVisualizer
         return model;
     }
 
-    public static LineSeries CreateLineSeries(string title, OxyColor color, LineStyle lineStyle = LineStyle.Solid, double thickness = 1)
+	/// <summary>
+	/// Erstellt eine <see cref="LineSeries"/> für ein Diagramm.
+	/// </summary>
+	/// <param name="title">Titel der Linie, der auch in der Legende erscheint.</param>
+	/// <param name="color">Farbe der Linie.</param>
+	/// <param name="lineStyle">Linienstil (z.B. Solid, Dash).</param>
+	/// <param name="thickness">Dicke der Linie.</param>
+	/// <returns>Eine konfigurierte <see cref="LineSeries"/>.</returns>
+	public static LineSeries CreateLineSeries(string title, OxyColor color, LineStyle lineStyle = LineStyle.Solid, double thickness = 1)
     {
         return new LineSeries
         {
@@ -47,14 +66,14 @@ public static class ChartVisualizer
         };
     }
 
-    /// <summary>
-    /// Erstellt eine horizontale Linie für den Durchschnitt einer Serie.
-    /// </summary>
-    /// <param name="history">Liste der History-Einträge.</param>
-    /// <param name="title">Titel der Linie.</param>
-    /// <param name="color">Farbe der Linie.</param>
-    /// <returns>LineSeries für die Durchschnittslinie.</returns>
-    public static LineSeries CreateAverageLine
+	/// <summary>
+	/// Erstellt eine horizontale Linie für den Durchschnitt einer Serie.
+	/// </summary>
+	/// <param name="history">Liste der History-Einträge mit Zeit, gewachsenen und verbrannten Bäumen.</param>
+	/// <param name="title">Titel der Linie, der in der Legende angezeigt wird.</param>
+	/// <param name="color">Farbe der Durchschnittslinie.</param>
+	/// <returns>Eine <see cref="LineSeries"/> für die Durchschnittslinie.</returns>
+	public static LineSeries CreateAverageLine
         (List<(TimeSpan Time, uint Grown, uint Burned)> history, string title, OxyColor color)
     {
         var sum = 0f;
