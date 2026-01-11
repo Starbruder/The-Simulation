4bf0c22622fccc8ab16648192a6934d51ef96e8a
STARBRUDER\Nik <starbruder.mc@gmail.com>
2026-01-11 18:50:43 +0100

    Impl for later diagnostics (evaluations)

---
+++ TheSimulation/TheSimulation/Enums/FireEventType.cs
@@ -0,0 +1,10 @@
+﻿namespace TheSimulation;
+
+public enum FireEventType
+{
+    // TODO: In future versions map editor can place controlled burns
+    //PlannedControlledBurn,
+    //SelfIgnition,
+    Lightning,
+    ManualIgnition // via user interaction
+}
