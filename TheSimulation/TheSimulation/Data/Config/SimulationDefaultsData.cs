51105a6c926745deec2f7af1bcca0f02e0629a6b
STARBRUDER\Nik <starbruder.mc@gmail.com>
2026-02-02 18:05:05 +0100

    Added XML Doku for SimulationDefaultsData.cs

--- TheSimulation/TheSimulation/Data/Config/SimulationDefaultsData.cs
+++ TheSimulation/TheSimulation/Data/Config/SimulationDefaultsData.cs
@@ -1,8 +1,26 @@
 ﻿namespace TheSimulation;
 
+/// <summary>
+/// Enthält die globalen Standardwerte für die Initialisierung der Simulationsparameter.
+/// Diese Konstanten definieren das Standardverhalten der Simulation, sofern keine Benutzereinstellungen vorliegen.
+/// </summary>
 public static class SimulationDefaultsData
 {
-    public const SimulationSpeed DefaultSimulationSpeed = SimulationSpeed.Ultra;
-    public const TreeShapeType DefaultTreeShapeType = TreeShapeType.Ellipse;
-    public const WindDirection DefaultWindDirection = WindDirection.North;
+	/// <summary>
+	/// Die standardmäßige Simulationsgeschwindigkeit. 
+	/// Voreingestellt auf <see cref="SimulationSpeed.Ultra"/> für maximale Berechnungsfrequenz.
+	/// </summary>
+	public const SimulationSpeed DefaultSimulationSpeed = SimulationSpeed.Ultra;
+
+	/// <summary>
+	/// Die standardmäßige visuelle Form der Bäume im Renderer.
+	/// Voreingestellt auf <see cref="TreeShapeType.Ellipse"/>.
+	/// </summary>
+	public const TreeShapeType DefaultTreeShapeType = TreeShapeType.Ellipse;
+
+	/// <summary>
+	/// Die standardmäßige Windrichtung bei Simulationsstart.
+	/// Voreingestellt auf <see cref="WindDirection.North"/>.
+	/// </summary>
+	public const WindDirection DefaultWindDirection = WindDirection.North;
 }
