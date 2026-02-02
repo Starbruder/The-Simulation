b8be69129cd93ff839f1485a76e0222dfd69ca21
STARBRUDER\Nik <starbruder.mc@gmail.com>
2026-02-02 18:07:00 +0100

    Added XML Doku for TerrainConfig

--- TheSimulation/TheSimulation/Data/Config/Terrain/TerrainConfig.cs
+++ TheSimulation/TheSimulation/Data/Config/Terrain/TerrainConfig.cs
@@ -1,5 +1,12 @@
 ﻿namespace TheSimulation;
 
+/// <summary>
+/// Definiert die Einstellungen für die Geländegenerierung innerhalb der Simulation.
+/// Bestimmt, welche Landschaftselemente beim Initialisieren der Welt erzeugt werden.
+/// </summary>
+/// <param name="UseTerrainGeneration">Gibt an, ob ein prozedurales Gelände (z. B. Höhenunterschiede) generiert werden soll.</param>
+/// <param name="EnableWaterBodies">Bestimmt, ob Wasserflächen (Seen oder Flüsse) in der Simulation platziert werden.</param>
+/// <param name="EnableRocks">Gibt an, ob unbrennbare Hindernisse wie Felsen oder Gesteinsformationen generiert werden sollen.</param>
 public sealed record TerrainConfig
 (
     bool UseTerrainGeneration,
